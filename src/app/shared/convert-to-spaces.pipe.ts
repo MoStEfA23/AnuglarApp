41e8c159ba6639d095dc53d88b3c2e783edefc24
MoStEfA23 <mostefa.benhammadi@gmail.com>
2020-03-08 18:20:38 +0100

    add custom pipe , to remove dash from productCode

---
+++ src/app/shared/convert-to-spaces.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+
+@Pipe({
+    name: 'convertToSpacesPipe'
+})
+export class convertToSpacesPipe implements PipeTransform {
+    transform(value: string, character: string): string{
+        return value.replace(character, ' ');
+    }
+}
